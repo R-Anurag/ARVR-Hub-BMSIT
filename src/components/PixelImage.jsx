d4cb7a38dd63860f711002eefcb0f68a8bf1e84f
Anurag Rai <146112209+R-Anurag@users.noreply.github.com>
2025-09-18 00:22:37 +0530

    testing out assets file paths issue

--- src/components/PixelImage.jsx
+++ src/components/PixelImage.jsx
@@ -4,7 +4,7 @@ export function PixelImageDemo() {
   return (
     <div className="flex justify-center items-center w-full h-full">
       <PixelImage
-        src="/events/core.png"
+        src="events/core.png"
         customGrid={{ rows: 12, cols: 16 }} // high detail
         grayscaleAnimation
         className="max-w-full max-h-full object-contain rounded-lg"
