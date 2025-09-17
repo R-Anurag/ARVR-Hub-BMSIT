38621913ab63c2cdb2b87cb2eac071649b765870
Anurag Rai <146112209+R-Anurag@users.noreply.github.com>
2025-09-18 01:16:36 +0530

    Update OrganizationFile.jsx

--- src/components/OrganizationFile.jsx
+++ src/components/OrganizationFile.jsx
@@ -269,14 +269,8 @@ function handleSelect(payload) {
         <div className="relative z-10 w-full h-[400px] rounded-lg border border-gray-700 p-4 overflow-visible">
           <Tree
             className="h-full w-full overflow-auto rounded-md bg-transparent p-2"
-            initialSelectedId="7"
-            initialExpandedItems={[
-              "1","2","3","4","5","6","7","8","9","10","11",
-              "12","13","14","15","16","17","18","19","20","21",
-              "22","23","24","25","26","27","28","29","30","31",
-              "32","33","34","35","36","37","38","39","40","41",
-              "42","43","44","45"
-            ]}
+            initialSelectedId="20"
+            initialExpandedItems={[]}
             elements={ELEMENTS}
             onSelect={handleSelect} // hook into selection
           >
