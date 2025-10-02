89b6e207a2a32064db86d873c547036c8ef7b6a0
marmikjain5 <marmikjain53@gmail.com>
2025-10-02 20:34:51 +0530

    removing individual gradient

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,3 @@
-import Hero from '../components/Hero'
-import CharacterSequence from '../components/CharacterSequence'
-import MarqueeText from "../components/MarqueeText";
-import About from '../components/About'
-import Benefits from '@/components/Benefits';
-import FooterARVR from '@/components/Footer';
-
 export default function Home() {
   return (
     <main className="relative bg-black text-white">
@@ -13,13 +6,17 @@ export default function Home() {
       <MarqueeText />
       <About />
 
-      {/*Gradient starts from Benefits + Footer */}
-      <div className="relative">
-        <div className="absolute inset-0 -z-10 bg-[radial-gradient(circle_at_center,_rgba(125,60,255,0.55)_0%,_rgba(0,0,0,1)_100%)]"></div>
-        
+      {/* ✅ Single continuous gradient for Benefits + Footer */}
+      <section className="relative">
+        {/* Gradient sits behind both Benefits & Footer */}
+        <div className="absolute inset-0 -z-10 h-full w-full 
+          bg-[radial-gradient(circle_at_center,_rgba(125,60,255,0.5)_0%,_rgba(0,0,0,1)_100%)] 
+          pointer-events-none">
+        </div>
+
         <Benefits />
         <FooterARVR />
-      </div>
+      </section>
     </main>
   )
 }
